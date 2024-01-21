0b5471d69904d3afbcb68a920ad533eba8bcb2dd
SifatNiloy <SifatNiloy18@gmail.com>
2024-01-21 22:19:30 +0600

    all packages installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+      <h2 className="">hello</h2>
+    </>
+  );
+}
+
+export default App;
