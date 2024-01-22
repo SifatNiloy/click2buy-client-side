69f83cb11cb7002f3eb7a1b64e5945c5f1991b0e
SifatNiloy <SifatNiloy18@gmail.com>
2024-01-22 23:53:27 +0600

    reviews component done

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,8 +1,8 @@
 const SectionTitle = ({ heading, subheading }) => {
   return (
     <div className="mx-auto text-center md:w-4/12 my-8">
-      <p className="text-yellow-600 mb-2">--{subheading}--</p>
       <p className="text-3xl uppercase border-y-4 py-4">--{heading}--</p>
+      <p className="text-yellow-600 mb-2">--{subheading}--</p>
     </div>
   );
 };
--- src/pages/Home/Reviews.jsx
+++ src/pages/Home/Reviews.jsx
@@ -1,12 +1,48 @@
+import SectionTitle from "../../components/SectionTitle";
+
+import React, { useEffect, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+// react rating
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
+// import required modules
+
+import { Navigation } from "swiper/modules";
 const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("/reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  });
   return (
-    <div className="text-center">
-      <div>
-        <h2 className="text-4xl">Write your review here</h2>
-      </div>
-      <div>
-        <h2 className="text-4xl text-center">Customer reviews</h2>
-      </div>
+    <div>
+      <SectionTitle
+        heading="Testimonials"
+        subheading="What Our Clients Say"
+      ></SectionTitle>
+      <p>{reviews.length}</p>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review.id}>
+            <div className="m-24 flex flex-col items-center">
+              <Rating
+                className="text-center"
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+              <p>{review.description}</p>
+              <h2 className="text-lg">{review.name}</h2>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
