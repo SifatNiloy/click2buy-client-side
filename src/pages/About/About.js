4ae3697189d71274fe52ec56bb8244df408cfef6
SifatNiloy <SifatNiloy18@gmail.com>
2024-01-22 20:52:33 +0600

    about component created

---
+++ src/pages/About/About.js
@@ -0,0 +1,48 @@
+const About = () => {
+  return (
+    <div className="mx-6">
+      <h2 className="text-4xl my-24">
+        Experience Personalized Online Shopping in Bangladesh with Click2buy
+      </h2>
+      <h5 className="text-sm my-12 text-left">
+        Online Shopping BD has never been easier. Click2Buy.com.bd is best
+        online shopping store in Bangladesh that features 10+ million products
+        at affordable prices. As bangaldesh online shopping landscape is
+        expanding every year, online shopping in dhaka, chittagong, khulna,
+        sylhet and other big cities are also gaining momentum. Click2Buy is
+        among best websites for online shopping in bangladesh that promises
+        fast, reliable and convenient delivery of products to your doorstep.
+        Click2Buy being the trusted online shop in Bangladesh aims to provide a
+        trouble-free shopping experience for the people of Bangladesh but is
+        also providing ample opportunity for international online shopping from
+        Bangladesh. Click2Buy aims to make online shopping accessible to all
+        parts of the country.
+      </h5>
+      <h5 className="text-sm text-left">
+        Everyone is encouraged to shop with confidence at Click2Buy.com.bd as
+        our strict buyers protection policies ensure no risks while shopping
+        online.
+      </h5>
+      <h5 className="text-sm text-left my-12">
+        Among tons of online stores in Bangladesh, Click2Buy aims to strictly
+        adhere to international quality standards ensuring trust and reliability
+        in customer service and originality in product delivery. Realizing the
+        surge in ecommerce platform in bangladesh, Click2Buy aims to redefine
+        online shopping experience with home delivery,cash on delivery and
+        installment facility for shoppers across country. Our assortment
+        includes 100% original products from leading electronics, fashion,
+        beauty, and lifestyle brands. Especially for shoppers who do not have
+        debit cards or credit cards, Click2Buy provides a facility of online
+        shopping with cash on delivery to your home. To enhance your online
+        shopping experience, we encourage you to download Click2Buy app for
+        Android & IOS and have a taste of personalized shopping experience based
+        on your interest and shopping needs. Watch BPL live on Click2Buy App
+        free. You can also check Click2Buy online shopping videos on YouTube to
+        have a clear idea. Click2Buy is a global online marketplace with
+        ecommerce stores in Pakistan, Bangladesh, Nepal, Sri Lanka and Myanmar.
+      </h5>
+    </div>
+  );
+};
+
+export default About;
