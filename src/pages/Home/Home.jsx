3360e2c0e3bf1c22bc150b0cbb8e1a259ece2f73
SifatNiloy <SifatNiloy18@gmail.com>
2024-01-23 15:48:45 +0600

    react helmet added for page title

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import Banner from "./Banner";
 import Brands from "./Brands";
 import Catagories from "./Catagories";
@@ -6,6 +7,9 @@ import Reviews from "./Reviews";
 const Home = () => {
   return (
     <div className="mx-auto container">
+      <Helmet>
+        <title>Click2buy | Home</title>
+      </Helmet>
       <Banner />
       <Catagories />
       <Brands />
--- src/pages/Products/HomeProducts.jsx
+++ src/pages/Products/HomeProducts.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const HomeProducts = ({ product }) => {
   const { name, seller, price, stock, ratings, img, quantity } = product;
   return (
