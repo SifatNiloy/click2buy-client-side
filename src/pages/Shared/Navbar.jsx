1b53b60a96ad6b9cb20f0abcceed92612c967edc
SifatNiloy <SifatNiloy18@gmail.com>
2024-01-24 13:21:04 +0600

    navbar done again

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
-
 const Navbar = () => {
   const navItems = (
     <>
@@ -23,14 +22,14 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-violet-600 h-20 text-white">
+    <div className="navbar bg-sky-200">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <Link to="/">
-              <img src={logo} alt="" />
-            </Link>
-          </label>
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden"
+          ></div>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
@@ -38,15 +37,18 @@ const Navbar = () => {
             {navItems}
           </ul>
         </div>
-        <Link to="/" className="btn btn-ghost normal-case text-xl">
+        <Link to="/">
           <img src={logo} alt="" />
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navItems}</ul>
       </div>
-      <div className="navbar-end pr-12"></div>
+      <div className="navbar-end">
+        <a className="btn">Login</a>
+      </div>
     </div>
+
   );
 };
 
