5fa5cb4b5fa8dd9a1c40f0ef5b20297b0cbc1fdc
SifatNiloy <SifatNiloy18@gmail.com>
2024-01-24 12:37:06 +0600

    shop page pagination added and search button implementation done

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,14 +1,43 @@
 import React, { useEffect, useState } from "react";
-import Products from "../Products/Products";
-import NotFound from "../Shared/NotFound";
 
+import "./Shop.css";
+import NotFound from "../Shared/NotFound";
+import Products from "../Products/Products";
 const Shop = () => {
+
   const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+  const pages = Math.ceil(count / size);
   useEffect(() => {
-    fetch("products.json")
+    const url = `http://localhost:5000/products?page=${page}&size=${size}`;
+    // console.log(page, size)
+    fetch(url)
       .then((res) => res.json())
-      .then((data) => setProducts(data));
-  });
+      .then((data) => {
+        setCount(data.count);
+        setProducts(data.products);
+      });
+  }, [page, size]);
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    var key = document.getElementById("myInput").value;
+
+    if (key) {
+      let result = await fetch(`http://localhost:5000/products/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      } else {
+        alert("Not found");
+      }
+    } else if (key == "") {
+      alert("enter a name");
+    }
+
+    document.getElementById("myInput").value = "";
+  };
   return (
     <div className="my-12">
       <div className="flex align-items-center justify-center">
@@ -20,7 +49,7 @@ const Shop = () => {
               className="input input-bordered w-96"
               id="myInput"
             />
-            <button className="btn btn-square">
+            <button className="btn btn-square" onClick={handleSearch}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
@@ -40,6 +69,7 @@ const Shop = () => {
         </form>
       </div>
       <div>
+        {/* <h2>products: {products?.length} </h2> */}
         <div className="grid grid-cols-4 justify-center gap-4">
           {products.length > 0 ? (
             products.map((product) => (
@@ -48,6 +78,25 @@ const Shop = () => {
           ) : (
             <NotFound />
           )}
+        </div>
+        {/* <p className='mt-12'>currently selected page: {page} and size :{size}</p> */}
+        <div className="pagination my-12 flex justify-center gap-6 ">
+          {[...Array(pages).keys()].map((number) => (
+            <button
+              className={page === number + 1 && "selected"}
+              key={number}
+              onClick={() => setPage(number + 1)}
+            >
+              {number + 1}
+            </button>
+          ))}
+          <select onChange={(event) => setSize(event.target.value)}>
+            <option value="5">5</option>
+            <option value="10" defaultValue={10}>
+              10
+            </option>
+            <option value="15">15</option>
+          </select>
         </div>
       </div>
     </div>
