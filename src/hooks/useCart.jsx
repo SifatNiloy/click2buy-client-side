e8dde4495f18bedc8e654d9293df891b1eb90023
SifatNiloy <SifatNiloy18@gmail.com>
2024-01-25 23:23:05 +0600

    useCart added

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,5 +1,26 @@
-const useCart = email=>{
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
 
-}
+const useCart = () => {
+  const { user } = useContext(AuthContext);
+  const {
+    isLoading,
+    refetch,
+    status,
+    data: cart = [],
+    error,
+  } = useQuery({
+    queryKey: ["orders", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/orders?email=${user?.email}`
+      );
+      return res.json();
+    },
+  });
+
+  return [cart, isLoading, refetch];
+};
 
 export default useCart;
